e8aa22acf8c38fa788057ab592fa6d8951db73fd
morkett <dcorkett@spartaglobal.co>
2017-03-24 16:59:19 +0000

    added comments

--- config/router.js
+++ config/router.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Users = require('../models/user-model');
 // var usersController = require('../controllers/users-controller');
-
+//delete this comment
 // TODO: fill in your router as required
 router.get('/users', function (req, res) {
   // TODO: this is where you'd get your Game data from your database
