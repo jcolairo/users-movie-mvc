f47fc00e816a11431b8c2e82a21e82e3585fe9d3
morkett <dcorkett@spartaglobal.co>
2017-03-25 13:16:31 +0000

    added seed data

--- db/seed.js
+++ db/seed.js
@@ -1,15 +1,53 @@
 // TODO: implement seedData()
 console.warn('NOTE! This is where you will seed your database.');
-console.warn('NOTE! Remember to change User & Movie to your primary & secondary model names... and the comments too!');
+console.warn('NOTE! Remember to change Game & Player to your primary & secondary model names... and the comments too!');
 
 var mongoose = require('mongoose');
 var User = require('../models/user-model');
-var Movie = require('../models/movie-model');
-var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/sg-webdev4-project2-movie';
+var Food = require('../models/movie-model');
+var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/sg-webdev4-project2';
 
 function seedData() {
-  console.warn('NOTE! seedData() needs to be implemented');
-  mongoose.connection.close();
+  var movie1 = new Food();
+  var movie2 = new Food();
+  var user = new User();
+  var menuSaved = [];
+
+  movie1.name = 'Krabby Patty';
+  movie1.course = 'Main';
+  movie1.price = '$100';
+  movie2.name = 'Krusty Krab Pizza';
+  movie2.course = 'Main';
+  movie2.price = '$50';
+
+  movie1.save(function (err, movie1Result) {
+    if (err) {
+      console.log('could not add', movie1.name ,'to the menu: err:', err);
+      process.exit(1);
+    }
+    menuSaved.push(movie1Result);
+    movie2.save(function (err, movie2Result) {
+      if (err) {
+        console.log('could not add', movie2.name ,'to the menu: err:', err);
+        process.exit(1);
+      }
+      menuSaved.push(movie2Result);
+      console.log('menuSaved:', menuSaved);
+      user.name = 'Krusty Krab';
+      user.location = 'Bikini Bottom';
+      user.cuisineStyle = 'Underwater American Glory';
+      user.menu.push(menuSaved[0]._id);
+      user.menu.push(menuSaved[1]._id);
+      user.save(function (err, userResult) {
+        if (err) {
+          console.log('could not create user: err:', err);
+          process.exit(1);
+        }
+        console.log('user saved:', userResult);
+        mongoose.connection.close();
+      });
+    });
+  });
 }
 
 function initDb() {
@@ -21,16 +59,16 @@ function initDb() {
     console.log('connected to', mongoose.connection.name);
     User.remove({}, function(err) {
       if (err) {
-        console.log('could not drop User collection: err:', err);
+        console.log('could not drop user collection: err:', err);
         process.exit(1);
       }
-      console.log('emptied User collection');
-      Movie.remove({}, function(err) {
+      console.log('emptied user collection');
+      Food.remove({}, function(err) {
         if (err) {
-          console.log('could not drop Movie collection: err:', err);
+          console.log('could not drop Food collection: err:', err);
           process.exit(1);
         }
-        console.log('emptied Movie collection');
+        console.log('emptied Food collection');
         seedData();
       });
     });
